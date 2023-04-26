3289184a42506e90ec499c6d5445a04200e2ca4e
Nathanael Smith <19243227+cecilsmith@users.noreply.github.com>
2023-04-26 03:37:19 -0500

    Update main.ino

--- code/main.ino
+++ code/main.ino
@@ -1,169 +1,243 @@
 #include <Arduino.h>
 #include "GPS_Air530.h"
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <stdio.h>
+#include "wiring_private.h"
 
-#define DEBUG 1            // set to 0 to disable debugging features (more efficient code)
-#define EXTERNAL_DISPLAY 1 // set to 0 to disable this feature
-
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
-
-// SoftwareSerial SoftSerial(2, 3);
-// SoftwareSerial GPSSerial(2, 3);
-
-#if EXTERNAL_DISPLAY == 1
-#define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-#endif
+static char recv_buf[512];
+static bool is_exist = false;
+static bool is_join = false;
 
 Air530Class Air530;
 
+// Parse the data strings from GPS
+
 double latitude, longitude;
 
-int parseGLLData(String GLLData);
-
-void setup()
-{
-#if DEBUG == 1
-  Serial.begin(9600);
-  delay(100);
-  Serial.println("Serial started");
-#endif
-
-#if EXTERNAL_DISPLAY == 1
-  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
-  {
-#if DEBUG == 1
-    Serial.println(F("SSD1306 allocation failed"));
-#endif
-  }
-  else
-  {
-    display.clearDisplay();
-    display.display();
-  }
-#endif
-
-  Air530.begin();
-  Air530.reset();
-
-  /* The modes supported are:
-   * GPS        :    MODE_GPS - this works
-   * GPS+BEIDOU :    MODE_GPS_BEIDOU this works
-   * GPS+GLONASS:    MODE_GPS_GLONASS this works
-   * GPS+GALILEO:    MODE_GPS_GALILEO this does not work
-   * GPS+BEIDOU+GLONASS+GALILEO:   MODE_GPS_MULTI   this does not work
-   * default mode is GPS+BEIDOU.
-   */
-
-  Air530.setmode(MODE_GPS_GLONASS); // was commented out
-#if (EXTERNAL_DISPLAY == 1) && (DEBUG == 1)
-  String sampleGLL = "$GNGLL,4458.02481,N,09313.50745,W,232623.000,A,A*58";
-  if (parseGLLData(sampleGLL))
-  {
-    display.clearDisplay();
-    display.setTextSize(1);              // Normal 1:1 pixel scale
-    display.setTextColor(SSD1306_WHITE); // Draw white text
-    display.setCursor(0, 0);             // Start at top-left corner
-    display.print("Lat: ");
-    display.println(latitude, 5);
-    display.print("Long: ");
-    display.println(longitude, 5);
-    display.display();
-  }
-#endif
-
-  String NMEA = Air530.getGSA();
-
-  /*supported nmea sentence :
-   * GLL, RMC, VTG, GGA, GSA, GSV
-   */
-  Air530.setNMEA(NMEA_GLL | NMEA_GSV | NMEA_VTG); // was commented out  and had  (NMEA_GGA|NMEA_GSA|NMEA_RMC|NMEA_VTG);
-
-  /* set PPS mode and pulse width.
-   *  void setPPS(uint8_t mode, uint16_t pulse_width = 500);
-   *  mode : 0, pps off;
-   *         1, first fixed;
-   *         2, 3D fixed;
-   *         3, 2D/3D fixed;
-   *         4, always on;
-   *  width : max value is 998 ms, default value is 500ms;
-   */
-  // Air530.setPPS(3, 500); // Was commented out
-#if DEBUG == 1
-  Serial.println(NMEA);
-#endif
-}
-
-void loop()
-{
-  /*get nmea sentence
-   * GPS.getNMEA() to get an any kind of nmea sentence;
-   * GPS.getRMC() to get RMC sentence;
-   * GPS.getGGA() to get GGA sentence;
-   * GPS.getGSA() to get GSA sentence;
-   * GPS.getGSV() to get GSV sentence;
-   * GPS.getGLL() to get GLL sentence;
-   * GPS.getVTG() to get VTG sentence;
-   */
-  // String NMEA = Air530.getNMEA();
-  String NMEA = Air530.getGLL();
-
-  if (parseGLLData(NMEA))
-  {
-#if EXTERNAL_DISPLAY == 1
-    display.clearDisplay();
-    display.setTextSize(1);              // Normal 1:1 pixel scale
-    display.setTextColor(SSD1306_WHITE); // Draw white text
-    display.setCursor(0, 0);             // Start at top-left corner
-    display.print("Lat: ");
-    display.println(latitude, 5);
-    display.print("Long: ");
-    display.println(longitude, 5);
-    display.display();
-#endif
-  }
-
-#if DEBUG == 1
-  if (NMEA != "0")
-  {
-    Serial.println(NMEA);
-  }
-#endif
-}
-
+// Comes out of module like this: "$GNGLL,4458.02481,N,09313.50745,W,232623.000,A,A*58"
 int parseGLLData(String GLLData)
 {
-  int latPoint = GLLData.indexOf('.');
-  int longPoint = GLLData.indexOf('.', latPoint + 1);
+    int latPoint = GLLData.indexOf('.');
+    int longPoint = GLLData.indexOf('.', latPoint + 1);
 
-  // Ensure data is there to be gleaned
-  if ((latPoint > 0) && (longPoint > 0) && (GLLData.length() > 28))
-  {
-    latitude = GLLData.substring(GLLData.indexOf(',') + 1, latPoint - 2).toDouble() + GLLData.substring(latPoint - 2, latPoint).toDouble() / 60.0 + GLLData.substring(latPoint, GLLData.indexOf(',', latPoint)).toDouble() / 36.0;
-
-    if (GLLData[GLLData.indexOf(',', latPoint) + 1] == 'S')
+    // Ensure data is there to be gleaned
+    if ((latPoint > 0) && (longPoint > 0) && (GLLData.length() > 28))
     {
-      latitude *= -1;
+        // Convert the string of hours, minutes, seconds, to decimal format
+        latitude = GLLData.substring(GLLData.indexOf(',') + 1, latPoint - 2).toDouble() + GLLData.substring(latPoint - 2, latPoint).toDouble() / 60.0 + GLLData.substring(latPoint, GLLData.indexOf(',', latPoint)).toDouble() / 36.0;
+
+        // Check if 'South' (negative latitude)
+        if (GLLData[GLLData.indexOf(',', latPoint) + 1] == 'S')
+        {
+            latitude *= -1;
+        }
+
+        // Convert the string of hours, minutes, seconds to decimal format
+        longitude = GLLData.substring(longPoint - 5, longPoint - 2).toDouble() + GLLData.substring(longPoint - 2, longPoint).toDouble() / 60.0 + GLLData.substring(longPoint, GLLData.indexOf(',', longPoint)).toDouble() / 36.0;
+
+        // Check if 'West' (negative longitude)
+        if (GLLData[GLLData.indexOf(',', longPoint) + 1] == 'W')
+        {
+            longitude *= -1;
+        }
+
+        Serial.print(latitude, 5);
+        Serial.print(", ");
+        Serial.println(longitude, 5);
+        return 1;
     }
+    else
+        return 0;
+}
 
-    longitude = GLLData.substring(longPoint - 5, longPoint - 2).toDouble() + GLLData.substring(longPoint - 2, longPoint).toDouble() / 60.0 + GLLData.substring(longPoint, GLLData.indexOf(',', longPoint)).toDouble() / 36.0;
+// --------------------------
 
-    if (GLLData[GLLData.indexOf(',', longPoint) + 1] == 'W')
+static int at_send_check_response(char *p_ack, uint16_t timeout_ms, char *p_cmd, ...)
+{
+    int ch;
+    int index = 0;
+    uint32_t startMillis = 0;
+    va_list args;
+    memset(recv_buf, 0, sizeof(recv_buf));
+    va_start(args, p_cmd);
+    Serial1.print(p_cmd);
+    Serial.print(p_cmd);
+    va_end(args);
+    delay(200);
+    startMillis = millis();
+
+    if (p_ack == NULL)
+        return 0;
+
+    do
     {
-      longitude *= -1;
-    }
+        while (Serial1.available() > 0)
+        {
+            ch = Serial1.read();
+            recv_buf[index++] = ch;
+            Serial.print((char)ch);
+            delay(2);
+        }
 
-#if DEBUG == 1
-    Serial.print(latitude, 5);
-    Serial.print(", ");
-    Serial.println(longitude, 5);
-#endif
-    return 1;
-  }
-  else
+        if (strstr(recv_buf, p_ack) != NULL)
+            return 1;
+
+    } while ((millis() - startMillis) < timeout_ms);
+    Serial.println();
     return 0;
+}
+
+static void recv_prase(char *p_msg)
+{
+    if (p_msg == NULL)
+    {
+        return;
+    }
+    char *p_start = NULL;
+    int data = 0;
+    int rssi = 0;
+    int snr = 0;
+
+    p_start = strstr(p_msg, "RX");
+    if (p_start && (1 == sscanf(p_start, "RX: \"%d\"\r\n", &data)))
+    {
+        Serial.println(data);
+    }
+
+    p_start = strstr(p_msg, "RSSI");
+    if (p_start && (1 == sscanf(p_start, "RSSI %d,", &rssi)))
+    {
+        Serial.print("rssi: ");
+        Serial.println(rssi);
+    }
+    p_start = strstr(p_msg, "SNR");
+    if (p_start && (1 == sscanf(p_start, "SNR %d", &snr)))
+    {
+        Serial.print("snr: ");
+        Serial.println(snr);
+    }
+    p_start = strstr(p_msg, "TX");
+    Serial.println(p_start);
+    if (p_start && (1 == sscanf(p_start, "+TEST: TX %s", &data)))
+    {
+        Serial.print("data: ");
+        Serial.println(data);
+    }
+}
+
+// ----------------------------------------------------------
+
+void setup(void)
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+
+    Serial.begin(9600); // Serial for debugging
+
+    Serial1.begin(9600); // Serial for LoRa
+
+    Air530.begin();
+    Air530.reset();
+    Air530.setmode(MODE_GPS_GLONASS); // was commented out
+
+    String sampleGLL = "$GNGLL,4458.02481,N,09313.50745,W,232623.000,A,A*58";
+    if (parseGLLData(sampleGLL))
+    {
+        Serial.print("Lat: ");
+        Serial.println(latitude);
+        Serial.print("Long : ");
+        Serial.println(longitude);
+    }
+
+    String NMEA = Air530.getGSA();
+    Air530.setNMEA(NMEA_GLL | NMEA_GSV | NMEA_VTG); // was commented out  and had  (NMEA_GGA|NMEA_GSA|NMEA_RMC|NMEA_VTG);
+    Serial.println(NMEA);
+
+    if (at_send_check_response("+AT: OK", 100, "AT\r\n"))
+    {
+        is_exist = true;
+        at_send_check_response("+ID: AppEui", 1000, "AT+ID\r\n");
+        at_send_check_response("+MODE: LWOTAA", 1000, "AT+MODE=LWOTAA\r\n");
+        at_send_check_response("+DR: US915", 1000, "AT+DR=US915\r\n");
+        at_send_check_response("+CH: NUM,1-8", 1000, "AT+CH=NUM,8-15\r\n");
+        at_send_check_response("+KEY: APPKEY", 1000, "AT+KEY=APPKEY,\"D657BA1D676CE69E428C57259FAB7B96\"\r\n");
+        at_send_check_response("+CLASS: A", 1000, "AT+CLASS=A\r\n");
+        at_send_check_response("+PORT: 8", 1000, "AT+PORT=8\r\n");
+        delay(200);
+        is_join = true;
+    }
+    else
+    {
+        is_exist = false;
+        Serial.print("No E5 module found.\r\n");
+    }
+}
+
+void loop(void)
+{
+    // Code for GPS data
+    String NMEA = Air530.getGLL();
+    if (parseGLLData(NMEA))
+    {
+        /*display.clearDisplay();
+        display.setTextSize(1);              // Normal 1:1 pixel scale
+        display.setTextColor(SSD1306_WHITE); // Draw white text
+        display.setCursor(0, 0);             // Start at top-left corner
+        display.print("Lat: ");
+        display.println(latitude, 5);
+        display.print("Long: ");
+        display.println(longitude, 5);
+        display.display();*/
+        Serial.print("Lat: ");
+        Serial.println(latitude);
+        Serial.print("Long : ");
+        Serial.println(longitude);
+    }
+
+    if (NMEA != "0")
+    {
+        Serial.println(NMEA);
+    }
+
+    // -----------------------------------------------
+
+    if (is_exist)
+    {
+        int ret = 0;
+        if (is_join)
+        {
+            // TURN OFF THE BUILT-IN LED
+            digitalWrite(LED_BUILTIN, LOW);
+
+            ret = at_send_check_response("+JOIN: Network joined", 12000, "AT+JOIN\r\n");
+            if (ret)
+            {
+                is_join = false;
+            }
+            else
+            {
+                at_send_check_response("+ID: AppEui", 1000, "AT+ID\r\n");
+                Serial.print("JOIN failed!\r\n\r\n");
+                delay(5000);
+            }
+        }
+        else
+        {
+            // TURN ON THE BUILT-IN LED ONCE JOINED
+            digitalWrite(LED_BUILTIN, HIGH);
+
+            char cmd[128];
+            sprintf(cmd, "AT+CMSGHEX=\"%04A%04A\"\r\n", (double)latitude, (double)longitude);
+            ret = at_send_check_response("Done", 5000, cmd);
+            if (ret)
+            {
+                recv_prase(recv_buf);
+            }
+            else
+            {
+                Serial.print("Send failed!\r\n\r\n");
+            }
+            delay(5000);
+        }
+    }
 }
